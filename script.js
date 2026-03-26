7830b467abfd987345aefa670aa5a571d02af30c
waxir <danishdarman06@gmail.com>
2026-03-26 21:18:10 +0500

    script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const expenseForm = document.getElementById("expense-form");
+    const expenseName = document.getElementById("expense-name");
+    const expenseAmount = document.getElementById("expense-amount");
+})

b899f115e93fb42f170630e427048dbde7a9f080
waxir <danishdarman06@gmail.com>
2026-03-27 10:17:36 +0500

    form submit js commited

--- script.js
+++ script.js
@@ -2,4 +2,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const expenseForm = document.getElementById("expense-form");
     const expenseName = document.getElementById("expense-name");
     const expenseAmount = document.getElementById("expense-amount");
+    const expenseList = document.getElementById("expense-list");
+    const totalAmountDisplay = document.getElementById("total-amount");
+
+    let expenses = []
+    let totalAmount = calculateTotal()
+
+
+    expenseForm.addEventListener('submit', (e)=>{
+        e.preventDefault()
+        const name = expenseName.value.trim()
+        const amount = parseFloat(expenseAmount.value.trim())
+
+        
+    })
+    function calculateTotal(){
+
+    }
 })
